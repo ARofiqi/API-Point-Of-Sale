cbe0e24739c7e1d70a37922e449a0af2fc1752aa
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2025-02-07 10:52:41 +0700

    add env

--- db/database.go
+++ db/database.go
@@ -2,17 +2,39 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	DB, err = sql.Open("mysql", "root:@tcp(localhost:3306)/db_aro_shop")
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file")
 	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Database is not reachable:", err)
+	}
+
+	log.Println("Connected to database successfully")
 }
